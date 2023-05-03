7949428d919b7c550c692bda35392010c7c4d065
Bridget Mahy <biddy.mahy@gmail.com>
2023-05-03 16:05:07 +1200

    implements getStaticProps to render blogposts - also parses fox external api

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/Layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData, getFox } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  const fox = await getFox()
+  return {
+    props: {
+      allPostsData,
+      fox,
+    },
+  }
+}
+
+export default function Home({ allPostsData, fox }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +26,21 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <img className="h-60 w-60 object-cover" src={fox.image} />
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
